d6bc36bf1f227686c9720a992d72e48d7a264e22
trj <423618259@qq.com>
2019-03-17 19:48:16 +0800

    收入列表导出

--- src/main/java/com/common/system/controller/BillMgrController.java
+++ src/main/java/com/common/system/controller/BillMgrController.java
@@ -1,17 +1,27 @@
 package com.common.system.controller;
 
+import cn.afterturn.easypoi.entity.vo.NormalExcelConstants;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
+import cn.afterturn.easypoi.view.PoiBaseView;
 import com.common.system.entity.BillIncome;
-import com.common.system.entity.RcUser;
+import com.common.system.entity.BillIncome;
 import com.common.system.service.BillService;
 import com.common.system.util.PageBean;
 import com.github.pagehelper.PageInfo;
+import com.xiaoleilu.hutool.date.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 @Controller
 @RequestMapping("bills")
@@ -58,5 +68,19 @@ public class BillMgrController extends BaseController{
                                          @RequestParam(value = "length", defaultValue = "10") int pageSize) {
             PageInfo<BillIncome> pageInfo = billService.listForPage((start / pageSize) + 1, pageSize);
         return new PageBean<BillIncome>(pageInfo);
+    }
+
+    
+    @RequestMapping(value = "exportExcel", method = RequestMethod.GET)
+    public void exportExcel(ModelMap modelMap, HttpServletRequest request,
+                            HttpServletResponse response) {
+        PageInfo<BillIncome> result = billService.listForPage(null, null);
+        ExportParams params = new ExportParams("支出信息", null, ExcelType.XSSF);
+        modelMap.put(NormalExcelConstants.DATA_LIST, result.getList());
+        modelMap.put(NormalExcelConstants.CLASS, BillIncome.class);
+        modelMap.put(NormalExcelConstants.PARAMS, params);
+        String fileName = DateUtil.format(new Date(), DateUtil.NORM_DATETIME_PATTERN);
+        modelMap.put(NormalExcelConstants.FILE_NAME, "支出信息表:" + fileName);
+        PoiBaseView.render(modelMap, request, response, NormalExcelConstants.EASYPOI_EXCEL_VIEW);
     }
 }
